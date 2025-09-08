bfc164d4c578249bbab0d72219c43fc608e34e1e
soggydog26 <thedoucet32@gmail.com>
2025-09-08 18:17:09 -0400

    fixes

--- src/main/java/net/gavin/testmod/item/ModItems.java
+++ src/main/java/net/gavin/testmod/item/ModItems.java
@@ -1,4 +1,24 @@
 package net.gavin.testmod.item;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.gavin.testmod.TestMod;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
 
 public class ModItems {
+
+        public static final Item PINK_GARNET = registerItem("pink_garnet", new Item(new Item.Settings()));
+
+        private static Item registerItem(String name, Item item) {
+            return Registry.register(Registries.ITEM, Identifier.of(TestMod.MOD_ID, name), item);
+        }
+
+        public static void registerModItems() {
+            TestMod.LOGGER.info("Registering Mod Items for" + TestMod.MOD_ID);
+        }
+
+        ItemGroupEvents.ModifyEntries();
+
 }
